35dabfce8505e0241017cdd8861dd6af1487aa02
Leandro Correia Galdino <62770546+leandrogaldino@users.noreply.github.com>
2021-07-05 17:37:28 -0300

    Alterações

---
+++ ControlLibrary/CrudObject.vb
@@ -0,0 +1,17 @@
+﻿Public Class CrudObject
+    Public Sub New()
+    End Sub
+    Public Sub New(ByVal ID As Long)
+        Load()
+    End Sub
+    Public Function Load() As Boolean
+
+    End Function
+    Public Function Save() As Boolean
+
+    End Function
+    Public Function Delete() As Boolean
+
+    End Function
+
+End Class
